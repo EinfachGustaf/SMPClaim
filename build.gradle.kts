f0d24113c6cbe83d9888d5cec3a54f773b082f54
DasShark <git@julius.software>
2024-04-12 20:56:39 +0200

    Remove Shadow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     kotlin("jvm") version "1.9.23"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
     id("io.papermc.paperweight.userdev") version "1.5.11"
     id("xyz.jpenilla.run-paper") version "2.2.3"
 }
@@ -27,10 +26,10 @@ dependencies {
     compileOnly(libs.worldguard)
 
     // Database Drivers
-    implementation(libs.postgresql)
+    compileOnly(libs.postgresql)
 
     // Exposed
-    implementation(libs.bundles.exposed)
+    compileOnly(libs.bundles.exposed)
 }
 
 tasks.test {
