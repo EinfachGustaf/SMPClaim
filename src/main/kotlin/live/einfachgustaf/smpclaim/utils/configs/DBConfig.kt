13dd7ea25f804a21780ee40b2a3572d0ff29d29d
DinoMarlir <72864666+DinoMarlir@users.noreply.github.com>
2024-04-14 20:08:53 +0200

    change default storage type to 'LOCAL_DB'

--- src/main/kotlin/live/einfachgustaf/smpclaim/utils/configs/DBConfig.kt
+++ src/main/kotlin/live/einfachgustaf/smpclaim/utils/configs/DBConfig.kt
@@ -5,7 +5,7 @@ import live.einfachgustaf.smpclaim.utils.Config
 class DBConfig : Config("database.yml") {
     override fun init() {
         config.addDefault("configVersion", "1.2")
-        config.addDefault("type", "POSTGRES")
+        config.addDefault("type", "LOCAL_JSON")
         config.addDefault("database.url", "jdbc:postgresql://localhost:5432/smpclaim")
         config.addDefault("database.user", "smpclaim")
         config.addDefault("database.password", "someverysecurepassword")
